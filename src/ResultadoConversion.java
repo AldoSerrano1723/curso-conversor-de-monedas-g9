560c859ff9781780c3512f8ec7c8fdb8647432db
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-11-25 08:42:42 -0600

    crea el response ResultadoConversion: - obtener los datos de la API

---
+++ src/ResultadoConversion.java
@@ -0,0 +1,5 @@
+public record ResultadoConversion(String base_code,
+                                  String target_code,
+                                  double conversin_rate,
+                                  double conversion_result) {
+}
