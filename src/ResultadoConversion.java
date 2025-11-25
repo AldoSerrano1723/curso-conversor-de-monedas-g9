abf533c5c3c40c6b9d6e65b73ddad64a3185f15a
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-11-25 09:40:21 -0600

    actualiza el record ResultadoConversion

--- src/ResultadoConversion.java
+++ src/ResultadoConversion.java
@@ -1,5 +1,5 @@
 public record ResultadoConversion(String base_code,
                                   String target_code,
-                                  double conversin_rate,
+                                  double conversion_rate,
                                   double conversion_result) {
 }
